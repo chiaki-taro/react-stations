7609249f2650fef518712775065e0e1020ec9ff3
“Chiaki” <chias.926@gmail.com>
2023-09-13 22:42:51 +0900

    station4 clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // DO NOT DELETE
 
 import './App.css'
-
+import React, { useState } from 'react';
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
   return (
     <div>
-      <header>タロ氏の成長日記 </header>
+      <header>Dogアプリ</header>
+      <body>
+        <h2>犬の画像を表示するサイトです</h2>
+        <img src= {dogUrl}/>
+      </body>
     </div>
   )
 }
